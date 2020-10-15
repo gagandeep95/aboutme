aa7b3b7641ad6317d562fb0d4dc74c5fb8afee2e
Gagandeep <gagandeep.developer.95@gmail.com>
2020-10-15 02:37:57 -0400

    added the route feature in app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 
 import Home from './components/Home';
 function App() {
   return (
-    <div className="App">
-      <Login />
-      <Register />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
