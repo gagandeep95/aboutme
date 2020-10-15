c69ecdf10d76df005c60aaaaf79b3c6b7857986a
Gagandeep <gagandeep.developer.95@gmail.com>
2020-10-15 02:20:36 -0400

    organized the components and added css file for each component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login';
-import Register from './components/Register';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
 
-import Home from './components/Home';
+import Home from './components/home/Home';
 function App() {
   return (
     <div className="App">
