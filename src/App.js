74330f5ef9fa60f598bd0e9eec792b27a75f08b3
Gagandeep <gagandeep.developer.95@gmail.com>
2020-10-15 02:07:28 -0400

    added home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login';
-
+import Home from './components/Home';
 function App() {
   return (
     <div className="App">
       <Login />
+      <Home />
     </div>
   );
 }
